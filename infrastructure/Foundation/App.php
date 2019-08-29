91c7114b209751c2ba2a6faa0967cf95639448d2
Shaun Clift <scliftas@gmail.com>
2019-08-29 18:24:56 +0100

    Added docblock to app class

--- infrastructure/Foundation/App.php
+++ infrastructure/Foundation/App.php
@@ -7,6 +7,11 @@ use Infrastructure\Router;
 
 class App
 {
+    /**
+     * Start a new instance of the whole application
+     *
+     * @return void
+     */
     public static function boot(): void
     {
         require_once __DIR__ . '/../../app/Routes/routes.php';
